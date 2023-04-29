bda244a5b524a7e330f92df2c0343a3eb22bef91
juha <juha@juhaui-MacBookPro.local>
2023-04-29 17:53:43 +0900

    CONTINUE:SQUARE

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export default
 function Button9() {
 	const square9 = [];
 	for (let i = 0; i < 3; i++) {
-		square9.push(<Squares/>)
+		square9.push(<Squares a={i*3} />)
 	}
 	return (
 		square9
--- src/Squares.js
+++ src/Squares.js
@@ -1,9 +1,17 @@
-export default function Squares() {
+export default function Squares({a}) {
+	function Square({a}) {
+		return (
+			<button className="square" onClick={onClickHandler}> {a} </button>
+		);
+	}
 	return (
 		<div>
-		<button className="square" onClick={() => alert("Click!")}> x </button>
-		<button className="square" onClick={() => alert("Click!")}> x </button>
-		<button className="square" onClick={() => alert("Click!")}> x </button>
+			<Square a={a + 1} />
+			<Square a={a + 2} />
+			<Square a={a + 3} />
 		</div>
 	);
+}
+
+function onClickHandler() {
 }
